3dd316b9f33079984fb36f0072c98f6910289efc
ipmianon170.@gmail.com <ipmianon170>
2020-12-12 05:11:54 -0500

    remove more TF logs

--- interact/__init__.py
+++ interact/__init__.py
@@ -1,7 +1,9 @@
 # suppress tensorflow logs
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 tf.logging.set_verbosity(tf.logging.ERROR)
 
 
